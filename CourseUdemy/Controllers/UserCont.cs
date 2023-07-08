8f8935a8ac57c2a32d4b1ae214d155c150780330
Isaac Essam <ishakessam122@gmail.com>
2023-07-08 12:32:42 +0300

    Add project files.

---
+++ CourseUdemy/Controllers/UserCont.cs
@@ -0,0 +1,29 @@
+﻿using CourseUdemy.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace CourseUdemy.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class UserCont : ControllerBase
+    {
+       
+        private readonly UserDbContext dataDbContext;
+
+        public UserCont ( UserDbContext dataDbContext )
+        {
+            this.dataDbContext = dataDbContext;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers ( )
+        {
+            return Ok (dataDbContext.users.ToList ()); 
+        }
+        [HttpGet("{id}")]
+
+        public ActionResult<User> GetOne (int id )
+        {
+            return  dataDbContext.users.Find (id);
+        }
+    }
+}
---
+++ CourseUdemy/Entity/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseUdemy.Entity
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext ( DbContextOptions options ) : base (options)
+        {
+        }
+        public  DbSet<User> users { get; set; }
+    }
+}
